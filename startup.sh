a185cb88da55f87a2e37bc0a9891c7998b466648
Labutin Evgeniy <labeg@mail.ru>
2020-05-03 16:36:06 +0300

    check container restart

--- startup.sh
+++ startup.sh
@@ -4,6 +4,8 @@
 set SCREEN_RESOLUTION 1920x1080
 catch {set SCREEN_RESOLUTION $env(resolution)}
 
+system "rm -fr /tmp/.X11-unix/*; rm -fr /tmp/X1-lock; rm -fr /home/headless/.Xauthority; rm -fr /home/headless/.vnc/*.log; rm -fr /home/headless/.vnc/*.pid"
+
 spawn /usr/bin/vncserver :1 -fg -geometry $SCREEN_RESOLUTION
 expect "Password:"
 send "$env(password)\r"
