8ce18faeb85ff1e5e5893dbaae6ae6ee5f7edb5b
Labutin Evgeniy <labeg@mail.ru>
2020-05-04 07:18:33 +0300

    fix clean tmp folder after stop container

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 set SCREEN_RESOLUTION 1920x1080
 catch {set SCREEN_RESOLUTION $env(resolution)}
 
-system "rm -fr /tmp/.X11-unix/*; rm -fr /tmp/X1-lock; rm -fr /home/headless/.Xauthority; rm -fr /home/headless/.vnc/*.log; rm -fr /home/headless/.vnc/*.pid"
+system "rm -fr /tmp/.X11-unix/*; rm -fr /tmp/.X1-lock; rm -fr /home/headless/.Xauthority; rm -fr /home/headless/.vnc/*.log; rm -fr /home/headless/.vnc/*.pid"
 
 spawn /usr/bin/vncserver :1 -fg -geometry $SCREEN_RESOLUTION
 expect "Password:"
